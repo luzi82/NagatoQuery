f099728aed9e74f377602189100f3dc16b322bf8
luzi82 <luzi82@gmail.com>
2013-01-11 20:21:31 +0800

    add tmp set

--- src/com/luzi82/nagatoquery/UtilCommand.java
+++ src/com/luzi82/nagatoquery/UtilCommand.java
@@ -9,6 +9,17 @@ public class UtilCommand {
 	public static void cmd_exit(NagatoQuery aQuery, NagatoQuery.CommandListener aListener) {
 		aQuery.setVar("%exit", "1");
 		aQuery.onExit();
+		aListener.commandReturn(null);
+	}
+
+	public static void cmd_set(NagatoQuery aQuery, NagatoQuery.CommandListener aListener, String aVarId, String aValue) {
+		aQuery.setVar("$" + aVarId, aValue);
+		aListener.commandReturn(null);
+	}
+
+	public static void cmd_settmp(NagatoQuery aQuery, NagatoQuery.CommandListener aListener, String aVarId, String aValue) {
+		aQuery.setVar("%" + aVarId, aValue);
+		aListener.commandReturn(null);
 	}
 
 }
